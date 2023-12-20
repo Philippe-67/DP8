dbe1abb2838a72723bc377789128e2e86a147a65
Philippe CHARLES <A114SR@login.axa>
2023-12-20 23:10:30 +0100

    dbContext

--- ServiceAgendaPraticien/Data/AgendaPraticienDbContext.cs
+++ ServiceAgendaPraticien/Data/AgendaPraticienDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace ServiceAgendaPraticien.Data
+﻿using ServiceAgendaPraticien.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ServiceAgendaPraticien.Data
 {
     public class AgendaPraticienDbContext
     {
+         
+        public DbSet<Agenda> Agenda { get; set; }
+        public DbSet<Praticien> Praticiens { get; set; }
+
+      
     }
 }
+
--- ServiceAgendaPraticien/obj/Debug/net6.0/ServiceAgendaPraticien.AssemblyInfo.cs
+++ ServiceAgendaPraticien/obj/Debug/net6.0/ServiceAgendaPraticien.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceAgendaPraticien")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6410fbd1de2f92ac53c656b5902354e3ca876d2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceAgendaPraticien")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceAgendaPraticien")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
