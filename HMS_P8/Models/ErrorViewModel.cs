bc2c98fa0ee32ff6b8f990a04edeec0d1a4e68fd
Philippe CHARLES <A114SR@login.axa>
2023-12-20 22:00:02 +0100

    vu

---
+++ HMS_P8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HMS_P8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
